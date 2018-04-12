c5100a172536e0913566e25236d6176893b1cd59
Natalya Furmanova <natalyafurmanova@Natalyas-MBP.fritz.box>
2018-04-12 11:03:46 +0200

    implements landing page

---
+++ src/components/Landing.vue
@@ -0,0 +1,42 @@
+<template id="Landing">
+<div>
+<div class="containermain">
+<img class="background" src="../assets/HomePage.jpg"></img>
+</div>
+<div class='link_container'>
+<router-link tag="a" to="/Register" class='button'>
+</router-link>
+</div>
+</div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+
+.containermain{
+  width: 100%;
+  position: relative;
+  height: 1780px;
+}
+
+.background {
+  object-fit: none;
+}
+
+.link_container{
+position: relative;
+  text-align: center;
+  width: 100%;
+}
+
+.button {
+display: inline-block;
+height: 115px;
+width: 400px;
+opacity: 0;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,13 +2,14 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Register from '@/components/register'
 import Profile from '@/components/Profile'
+import Landing from '@/components/Landing'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
+      path: '/register',
       name: 'Register',
       component: Register
     },
@@ -16,6 +17,11 @@ export default new Router({
       path: '/profile',
       name: 'Mona Lisa',
       component: Profile
+    },
+    {
+      path: '/',
+      name: 'Landing',
+      component: Landing
     }
   ]
 })
